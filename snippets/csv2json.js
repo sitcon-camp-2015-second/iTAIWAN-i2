0f37cdfcc142ffd894da4a84aaf83c2802077ec9
Andy Pan <andy0130tw@yahoo.com.tw>
2015-08-05 03:05:32 +0800

    snippet: csv2json with a test case

---
+++ snippets/csv2json.js
@@ -0,0 +1,30 @@
+
+var rexQuoteStrip = /^\s*"(.*)"\s*$/;
+
+function csv2json(csv, headers) {
+  var rtn = [];
+  var rows = csv.split('\n');
+  var row;
+  var obj;
+  var matched;
+  for (var i = 0, c = rows.length; i < c; i++) {
+    row = rows[i].split(',');
+    obj = {};
+    for (var j = 0, d = headers.length; j < d; j++) {
+      if (row[j] == null)
+        continue;
+      if (matched = row[j].match(rexQuoteStrip)) {
+        row[j] = matched[1];
+      } else if (!isNaN(matched = parseFloat(row[j]))) {
+      	// convert to number if possible
+      	row[j] = matched;
+      }
+      obj[headers[j]] = row[j];
+    }
+    rtn.push(obj);
+  }
+  return rtn;
+}
+
+// test case:
+// csv2json('111,222,333,"444"\n555,666,777,"888"\n,1,2,3', ['A','B','C','D'])
