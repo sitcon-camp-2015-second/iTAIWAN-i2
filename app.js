a343faa0cc300fc927ae78133f241f449896c802
Andy Pan <andy0130tw@yahoo.com.tw>
2015-08-05 20:41:13 +0800

    good looking clusters

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
 
-var rexQuoteStrip = /^\s*"(.*)"\s*$/;
-
-function csv2json(csv, headers) {
-  var rtn = [];
-  var rows = csv.split('\n');
-  var row;
-  var obj;
-  var matched;
-  for (var i = 0, c = rows.length; i < c; i++) {
-    row = rows[i].split(',');
-    obj = {};
-    for (var j = 0, d = headers.length; j < d; j++) {
-      if (row[j] == null)
-        continue;
-      if (matched = row[j].match(rexQuoteStrip)) {
-        row[j] = matched[1];
-      } else if (!isNaN(matched = parseFloat(row[j]))) {
-      	// convert to number if possible
-      	row[j] = matched;
-      }
-      obj[headers[j]] = row[j];
-    }
-    rtn.push(obj);
+var mapConf = {
+  boundary: [[21.5, 119], [25.5, 123]],
+  clusterConf: {
+  	disableClusteringAtZoom: 14
   }
-  return rtn;
-}
+};
+
+(function(rexQuoteStrip) {
+  csv2json = function(csv, headers) {
+    var rtn = [];
+    var rows = csv.split('\n');
+    var row;
+    var obj;
+    var matched;
+    for (var i = 0, c = rows.length; i < c; i++) {
+      row = rows[i].split(',');
+      obj = {};
+      for (var j = 0, d = headers.length; j < d; j++) {
+        if (row[j] == null)
+          continue;
+        if (matched = row[j].match(rexQuoteStrip)) {
+          row[j] = matched[1];
+        } else if (!isNaN(matched = parseFloat(row[j]))) {
+          // convert to number if possible
+          row[j] = matched;
+        }
+        obj[headers[j]] = row[j];
+      }
+      rtn.push(obj);
+    }
+    return rtn;
+  }
+})(/^\s*"(.*)"\s*$/);
 
 // test case:
 // csv2json('111,222,333,"444"\n555,666,777,"888"\n,1,2,3', ['A','B','C','D'])
+
+var hotspotColl = null;
+var hotspotList = [];
+var hotspotMarkers = new L.MarkerClusterGroup(mapConf.clusterConf);
 
 $.ajax({
   url: 'hotspotlist.csv',
   success: function(resp) {
 	//console.log(resp);
-    console.log(csv2json(resp, ["owner","area","name","addr","lng","lat"]))
+    hotspotColl = csv2json(resp, ["owner","area","name","addr","lng","lat"]);
+    var hs, mkr;
+    // add the icon
+    for (var i = 0, c = hotspotColl.length; i < c; i++) {
+      hs = hotspotColl[i];
+      mkr = new L.marker([hs.lng, hs.lat], { title: hs.name });
+      mkr.bindPopup('<h4 class="title">' + hs.name + '</h4><p class="address">' + hs.addr + '</p>');
+      hotspotList.push(mkr);
+      // L.marker([hs.lng, hs.lat]).addTo(map);
+    }
+    hotspotMarkers.addLayers(hotspotList);
+    map.addLayer(hotspotMarkers);
   }
 });
-
-var mapConf = {
-  boundary: [[21.5, 119], [25.5, 123]]
-};
-
 
 // load OSM
 var map = L.map('mapContainer', {
